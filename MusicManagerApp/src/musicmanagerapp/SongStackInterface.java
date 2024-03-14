cdd0ddac1221a369fde651e4c8bba9280191ca56
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-03-14 11:36:07 +0000

    I have changed the algorithim I am using

---
+++ MusicManagerApp/src/musicmanagerapp/SongStackInterface.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package musicmanagerapp;
+
+/**
+ *
+ * @author dmoc2
+ */
+public interface SongStackInterface {
+    
+    public void push(Object newSong);
+    public Object pop();
+    public Object peek();
+    
+    public boolean isEmpty();
+    public int size();
+    public String displayLikedSongs();
+    
+}
