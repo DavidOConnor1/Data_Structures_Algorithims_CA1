51eade97b526b9eb4999b5ab5673dcad09e56bc2
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-09-18 12:03:11 +0100

    loop loop loop

--- InputMismatchException/src/main/java/com/mycompany/inputmismatchexception/TryGuess.java
+++ InputMismatchException/src/main/java/com/mycompany/inputmismatchexception/TryGuess.java
@@ -17,36 +17,43 @@ public class TryGuess {
         
         int a=0, b=0;
         int sum;
+        boolean notOk = false;
         
-         try
-        {
+        
             do{
-                System.out.println("The correct number is between 1 & 10");
-                System.out.println("Enter Two numbers to add");
-                a = input.nextInt();
-                b = input.nextInt();
+                 try
+                {
+                    System.out.println("The correct number is between 1 & 10");
+                    System.out.println("Enter Two numbers to add");
+                    a = input.nextInt();
+                    b = input.nextInt();
             
-                System.out.println("Number"+a+"+"+b);
+                 System.out.println("Number"+a+"+"+b);
             
-                sum = a+b;
+                 sum = a+b;
             
-                System.out.println("The sum of your numbers are: "+sum);
+                 System.out.println("The sum of your numbers are: "+sum);
             
                 if(sum == 10){
                  System.out.println("Congratulations you got the number!");
                  break;
-             }
-            
-            } while(true); //continues the do until the correct number is guessed
+             } else {
+                    System.out.println("Try again");
+                    notOk = true;
+                }
+                } catch(InputMismatchException ex){
+             System.out.println("That is not a number, try again");
+             
+             System.out.println(ex);
+             
+        }
+            } while(notOk); //continues the do until the correct number is guessed
             
         
             
             
-        } 
-         catch(InputMismatchException ex){
-             
-             System.out.println(ex);
-        }
+        
+         
         
     }
 }
