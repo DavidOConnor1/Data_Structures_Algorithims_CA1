5378d69d4612b02e6dd4bad7e14a4640ff77f588
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-09-25 12:55:48 +0100

    Threads are threading

--- ThreadingDavey/src/main/java/com/mycompany/threadingdavey/ThatThreadTask5.java
+++ ThreadingDavey/src/main/java/com/mycompany/threadingdavey/ThatThreadTask5.java
@@ -10,4 +10,30 @@ package com.mycompany.threadingdavey;
  */
 public class ThatThreadTask5 {
     
+    public static void main(String[] args)
+    {
+        
+        for(int i=0; i<100; i++)
+        {
+            
+            Thread newThread = new Thread(() -> {
+                System.out.println("a");
+            });
+            newThread.start();
+            Thread newThreadTwo = new Thread(() -> {
+                System.out.println("b");
+                
+            });
+            newThreadTwo.start();
+            Thread newThreadThree = new Thread(() -> {
+                int count=0;
+                count++;
+                System.out.println(count);
+                
+            });
+            newThreadThree.start();
+        }
+       
+        
+    }
 }
